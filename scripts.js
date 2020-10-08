c2ef87f0af69d93c8a3a9aee8680e42d0d7c6c66
Felix Schmid <50250674+Felix-Schmid@users.noreply.github.com>
2020-10-08 11:15:32 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,11 +3,11 @@ var mobileMenuOpen = false;
 function toggleMobileMenu() {
 	if (mobileMenuOpen) {
 		document.getElementById("mobileMenu").style.height = "0";
-		document.getElementById("menuIcon").src = "Icons/menu.png";
+		document.getElementById("menuIcon").src = "icons/menu.png";
 	}
 	else {
 		document.getElementById("mobileMenu").style.height = "auto";
-		document.getElementById("menuIcon").src = "Icons/close.png";
+		document.getElementById("menuIcon").src = "icons/close.png";
 	}
 	mobileMenuOpen = !mobileMenuOpen;
 }
